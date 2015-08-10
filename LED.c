c850390357337ab1cfdb8a6415a973a9068da07d
Sanjay Shreedharan <shridharsanjay1994@gmail.com>
2015-08-10 23:38:16 +0530

    Comments Updated

--- LED.c
+++ LED.c
@@ -6,5 +6,5 @@ void main()
   OSCCONbits.IRCF1=1;
   OSCCONbits.IRCF2=1;
   TRISD=0x00; // Sets the PortD as output port
-  PORTD= 0b10101100; Sets the 3rd, 4th, 6th and 8th bit of PortD to high.
+  PORTD= 0b10101100; //Sets the 3rd, 4th, 6th and 8th bit of PortD to high.
 }
